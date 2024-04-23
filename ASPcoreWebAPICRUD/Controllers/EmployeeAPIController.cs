9fce8c6916312b3ddbd59e8926a56f3d0ef723dc
Sandeep Kaler <sandeepkaler19@gmail.com>
2024-04-23 18:26:12 +0530

    Add project files.

---
+++ ASPcoreWebAPICRUD/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,68 @@
+﻿using ASPcoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPcoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeAPIController : ControllerBase
+    {
+        private readonly OrganizationContext context;
+
+        public EmployeeAPIController(OrganizationContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployee()
+        {
+            var data = await context.Employees.ToListAsync();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        { 
+            var employee = await context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee(Employee emp)
+        {
+           await context.Employees.AddAsync(emp);
+            await context.SaveChangesAsync();
+            return Ok(emp);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee emp)
+        {
+            if(id != emp.EmployeeId)
+            {
+                return BadRequest();
+            }
+            context.Entry(emp).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(emp);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int id)
+        {
+            var emp = await context.Employees.FindAsync(id);
+            if(emp == null)
+            { 
+                return NotFound();
+            }
+            context.Employees.Remove(emp);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ ASPcoreWebAPICRUD/Models/OrganizationContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPcoreWebAPICRUD.Models;
+
+public partial class OrganizationContext : DbContext
+{
+    public OrganizationContext()
+    {
+    }
+
+    public OrganizationContext(DbContextOptions<OrganizationContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__employee__C52E0BA84DB08DE9");
+
+            entity.ToTable("employee");
+
+            entity.Property(e => e.EmployeeId)
+                .ValueGeneratedNever()
+                .HasColumnName("employee_id");
+            entity.Property(e => e.Department)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("department");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("first_name");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("last_name");
+            entity.Property(e => e.Position)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("position");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
